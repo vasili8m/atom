f7e5ff5de5d60fa9c0309b594f5cc7f1d7d465ad
darangi <small_musa@hotmail.com>
2021-05-06 10:37:00 +0100

    fix lint

--- script/vsts/get-release-version.js
+++ script/vsts/get-release-version.js
@@ -68,10 +68,10 @@ async function getReleaseVersion() {
 
   // Write out some variables that indicate whether artifacts should be uploaded
   const buildBranch = process.env.BUILD_SOURCEBRANCHNAME;
-  const isReleaseBranch = true
-    // process.env.IS_RELEASE_BRANCH ||
-    // // argv.nightly ||
-    // // buildBranch.match(/\d\.\d+-releases/) !== null;
+  const isReleaseBranch = true;
+  // process.env.IS_RELEASE_BRANCH ||
+  // // argv.nightly ||
+  // // buildBranch.match(/\d\.\d+-releases/) !== null;
   const isSignedZipBranch =
     !isReleaseBranch &&
     (process.env.IS_SIGNED_ZIP_BRANCH ||
